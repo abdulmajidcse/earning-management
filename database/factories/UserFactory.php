00e45257d3fb181b999a55cea9eade5b71409761
Abdul Majid <abdulmajidcse@gmail.com>
2024-01-23 15:44:50 +0600

    DB migrations. 1. Added transaction model and migration with necessary fields. 2. Update user model and migration. 3. Added Model Factory and Seeder. 4. Added phpmyadmin in docker composer.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,8 +26,9 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
+            'mobile' => fake()->phoneNumber(),
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'password' => static::$password ??= Hash::make('12345678'),
             'remember_token' => Str::random(10),
         ];
     }
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // create admin
+        User::factory()->create([
+            'email' => 'admin@gmail.com',
+            'mobile' => '01700000000',
+            'is_admin' => 1,
+        ]);
+
+        // create general user
+        User::factory(10)->create();
+    }
+}
